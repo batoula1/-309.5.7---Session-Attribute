0aae656d2913068d641d1e9b64c1a2352a3ab629
batoula1 <102437354+batoula1@users.noreply.github.com>
2024-08-28 23:07:10 -0400

    BeginProject

---
+++ src/main/java/com/Glab30957/SessionAttributes/SessionAttributesApplication.java
@@ -0,0 +1,13 @@
+package com.Glab30957.SessionAttributes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SessionAttributesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SessionAttributesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Glab30957/SessionAttributes/SessionAttributesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Glab30957.SessionAttributes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SessionAttributesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
